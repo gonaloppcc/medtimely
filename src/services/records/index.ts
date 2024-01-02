77d00a0ef79ac4bfcf69a147d3d24d9e9557d638
matildeopbravo <matildeopbravo@gmail.com>
2024-01-02 12:49:20 +0000

    Implemented getRecord

--- src/services/records/index.ts
+++ src/services/records/index.ts
@@ -5,7 +5,10 @@ import {
     collection,
     query,
     where,
-    onSnapshot,
+    doc,
+    DocumentReference,
+    DocumentSnapshot,
+    getDoc
 } from 'firebase/firestore';
 
 import {
@@ -17,6 +20,7 @@ import dayjs from 'dayjs'
 
 const SMALL_STALL_TIME = 1000;
 const RECORDS_COLLECTION = 'medicationRecords';
+const records = collection(db, RECORDS_COLLECTION);
 
 export const getRecords = async (
     token: string,
@@ -26,7 +30,6 @@ export const getRecords = async (
         `Fetching records in date=${date.toDateString()} for token=${token}`
     );
 
-    const records = collection(db, RECORDS_COLLECTION);
     // date is stored as timestamp in the db
     const startDate = dayjs(date).startOf('day').toDate();
     const endDate = dayjs(date).endOf('day').toDate();
@@ -34,19 +37,14 @@ export const getRecords = async (
     // TODO: get only records of current user
     const q = query(
         records,
+        //where('user', '==', token),
         where('scheduledTime', '<=', endDate),
         where('scheduledTime', '>=', startDate)
     );
     // todo: use onSnapshot
     const matchDocs = await getDocs(q);
 
-    const matchRecords: MedicationRecord[] = matchDocs.docs.map(
-        (doc) => {
-            const data = doc.data()
-            const scheduledTime = data.scheduledTime.toDate()
-            return {...data, scheduledTime, id: doc.id} as MedicationRecord
-        }
-    );
+    const matchRecords: MedicationRecord[] = matchDocs.docs.map(snapshotToRecord);
     console.log(matchRecords);
     return matchRecords;
 };
@@ -56,7 +54,7 @@ const RECORD: MedicationRecord = {
     form: MedicationRecordForm.TABLET,
     amount: 3,
     missed: true,
-    time: new Date(),
+    scheduledTime: new Date(),
 };
 export const getRecord = async (
     token: string,
@@ -64,11 +62,20 @@ export const getRecord = async (
 ): Promise<MedicationRecord> => {
     console.log(`Fetching record with id=${id} for token=${token}`);
 
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(RECORD);
-        }, SMALL_STALL_TIME);
-    });
+    const docRef: DocumentReference = doc(db, RECORDS_COLLECTION, id);
+    try {
+        const docSnap: DocumentSnapshot = await getDoc(docRef);
+        if (docSnap.exists()) {
+            return snapshotToRecord(docSnap)
+        }
+        else {
+            throw new Error(`No record with id=${id} exists`);
+        }
+    }
+    catch (e) {
+        console.log(e);
+        throw e;
+    }
 };
 
 export const createRecord = async (
@@ -82,7 +89,7 @@ export const createRecord = async (
     return new Promise((resolve) => {
         setTimeout(() => {
             // Create mock record creation here
-            const day = record.time.toISOString().split('T')[0];
+            const day = record.scheduledTime.toISOString().split('T')[0];
             MEDICATION_RECORDS_BY_DATE[day].push(record);
 
             resolve('CreatedID'); // TODO: Replace with actual ID
@@ -108,3 +115,14 @@ export const updateRecord = async (
         }, SMALL_STALL_TIME);
     });
 };
+
+const snapshotToRecord = (doc: DocumentSnapshot): MedicationRecord => {
+    if (doc.exists()) {
+        const data = doc.data()
+        const scheduledTime = data.scheduledTime.toDate()
+        return {...data, scheduledTime, id: doc.id} as MedicationRecord
+    }
+    else {
+        throw new Error("Document does not exist");
+    }
+}
