b3ec8a7eb15bafb6e17baaacc089c18cb0a11716
Pedro Alves <pta2002@pta2002.com>
2024-01-04 16:53:17 +0000

    material bottom bar

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,8 +1,51 @@
+import { CommonActions } from '@react-navigation/native';
 import { Tabs } from 'expo-router';
-import { Icon } from 'react-native-paper';
+import { BottomNavigation, Icon } from 'react-native-paper';
 
 export default function HomeLayout() {
-  return <Tabs>
+  return <Tabs
+    tabBar={({ navigation, state, descriptors, insets }) => (
+      <BottomNavigation.Bar
+        navigationState={state}
+        safeAreaInsets={insets}
+        onTabPress={({ route, preventDefault }) => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (event.defaultPrevented) {
+            preventDefault();
+          } else {
+            navigation.dispatch({
+              ...CommonActions.navigate(route.name, route.params),
+              target: state.key,
+            });
+          }
+        }}
+        renderIcon={({ route, focused, color }) => {
+          const { options } = descriptors[route.key];
+          if (options.tabBarIcon) {
+            return options.tabBarIcon({ focused, color, size: 24 });
+          }
+
+          return null;
+        }}
+        getLabelText={({ route }) => {
+          const { options } = descriptors[route.key];
+          const label =
+            options.tabBarLabel !== undefined
+              ? options.tabBarLabel as string
+              : options.title !== undefined
+                ? options.title
+                : route.name;
+
+          return label;
+        }}
+      />)
+    }
+  >
     <Tabs.Screen
       name="index"
       options={{
