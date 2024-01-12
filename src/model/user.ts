83f1b17cf9b0015738904fffd3aa8467a2ae49bf
matildeopbravo <matildeopbravo@gmail.com>
2024-01-12 12:06:18 +0000

    Fixed prettier problem

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,11 +1,11 @@
-import {MedicationRecord} from './medicationRecord';
-import {Medication} from './medication';
+import { MedicationRecord } from './medicationRecord';
+import { Medication } from './medication';
 
 export interface User {
-    id?: string,
-    firstname?: string,
-    lastname?: string,
-    records: MedicationRecord[]
-    medications: Medication[]
-    groups: string[]
+    id?: string;
+    firstname?: string;
+    lastname?: string;
+    records: MedicationRecord[];
+    medications: Medication[];
+    groups: string[];
 }
