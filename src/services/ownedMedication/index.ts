571ecac719671011802f16fd96f5e3acaed8582a
Gonçalo André Rodrigues Soares <62038187+gonaloppcc@users.noreply.github.com>
2024-01-12 18:05:04 +0000

    Add medicationRecord file again. Add getUserOwnedMedications and getUserGroupOwnedMedications mock functions.

--- src/services/ownedMedication/index.ts
+++ src/services/ownedMedication/index.ts
@@ -17,13 +17,49 @@ export const OWNED_MEDICATION: OwnedMedication = {
 };
 
 export const getOwnedMedication = async (
-    token: string
+    uid: string,
+    ownedMedicationId: string
 ): Promise<OwnedMedication> => {
-    console.log(`Fetching stock for user with id=${token}`);
+    console.log(
+        `Fetching stock for user with id=${uid} and ownedMedicationId=${ownedMedicationId}`
+    );
 
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve(OWNED_MEDICATION);
+        }, 1000);
+    });
+};
+
+// Only the user's owned medications are returned
+// Meaning the group ones are not returned
+export const getUserOwnedMedications = async (
+    uid: string
+): Promise<OwnedMedication[]> => {
+    console.log(`Fetching stock for user with id=${uid}`);
+
+    // TODO: Implement this
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([OWNED_MEDICATION]);
+        }, 1000);
+    });
+};
+
+export const getUserGroupOwnedMedications = async (
+    uid: string,
+    groupId: string
+): Promise<OwnedMedication[]> => {
+    console.log(
+        `Fetching stock for user with id=${uid} and groupId=${groupId}`
+    );
+
+    // TODO: Implement this
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([OWNED_MEDICATION]);
         }, 1000);
     });
 };
