779edb1b535730b3c10ab51e81d55c0ead826b7a
matildeopbravo <matildeopbravo@gmail.com>
2024-01-16 12:07:12 +0000

    Implemented createGroup and getGroup

--- src/services/groups/index.ts
+++ src/services/groups/index.ts
@@ -1,15 +1,21 @@
-import { Group } from '../../model/group';
+import { Group, GroupData } from '../../model/group';
 import { User } from '../../model/user';
 import {
     DocumentReference,
     DocumentSnapshot,
     doc,
     getDoc,
+    addDoc,
+    updateDoc,
+    collection,
+    arrayUnion,
 } from 'firebase/firestore';
 import { Firestore } from 'firebase/firestore';
+import { ProjectError } from '../error';
+//import { runTransaction } from 'firebase/firestore';
 
 const USERS_COLLECTION_NAME = 'users';
-//const GROUPS_COLLECTION_NAME = 'groups';
+const GROUPS_COLLECTION_NAME = 'groups';
 
 export const getUserGroups = async (
     db: Firestore,
@@ -38,7 +44,7 @@ export const getUserGroups = async (
             }
 
             const userRefInGroup = groupDocSnapshot.data()?.users || [];
-            const usersData: User[] = await getUsersInGroup(userRefInGroup);
+            const usersData: User[] = await getUsersByRef(userRefInGroup);
 
             return {
                 id: groupDocSnapshot.id,
@@ -50,7 +56,8 @@ export const getUserGroups = async (
     return groupsData;
 };
 
-const getUsersInGroup = async (
+// TODO: create getUser
+const getUsersByRef = async (
     userRefs: DocumentReference[]
 ): Promise<User[]> => {
     return Promise.all(
@@ -66,4 +73,48 @@ const getUsersInGroup = async (
             } as User;
         })
     );
+};
+
+export const getGroup = async (
+    db: Firestore,
+    groupId: string
+): Promise<Group> => {
+    const groupDocRef: DocumentReference = doc(
+        db,
+        GROUPS_COLLECTION_NAME,
+        groupId
+    );
+    const groupDocSnapshot: DocumentSnapshot = await getDoc(groupDocRef);
+
+    if (!groupDocSnapshot.exists()) {
+        throw new ProjectError(
+            'INVALID_GROUP_ID_ERROR',
+            `Group with id=${groupId} not found`
+        );
+    }
+
+    const userRefInGroup = groupDocSnapshot.data()?.users || [];
+    const usersData: User[] = await getUsersByRef(userRefInGroup);
+
+    return {
+        id: groupDocSnapshot.id,
+        ...groupDocSnapshot.data(),
+        users: usersData,
+    } as Group;
+};
+// TODO updateGroup, deleteGroup, addUserToGroup, removeUserFromGroup
+// Maybe store the user that created so that only that user can delete the group
+export const createGroup = async (
+    db: Firestore,
+    groupData: GroupData,
+    userId: string
+): Promise<string> => {
+    const userRef: DocumentReference = doc(db, USERS_COLLECTION_NAME, userId);
+    const groupRef = await addDoc(collection(db, GROUPS_COLLECTION_NAME), {
+        ...groupData,
+        users: [userRef],
+    });
+    await updateDoc(userRef, { groups: arrayUnion(groupRef) });
+
+    return groupRef.id;
 };
