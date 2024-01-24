434681d9adcd68316f07c7e85a1f6c51866ac2e8
Pedro Alves <pta2002@pta2002.com>
2024-01-24 21:06:28 +0000

    fix color

--- src/components/EmptyPlannedMedications.tsx
+++ src/components/EmptyPlannedMedications.tsx
@@ -1,19 +1,20 @@
 import { router } from 'expo-router';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Button, Icon, Text, useTheme } from 'react-native-paper';
+import { Button, Icon, Text } from 'react-native-paper';
+import { useAppTheme } from '../theme';
 
 export const EmptyPlannedMedications = () => {
-    const theme = useTheme();
+    const theme = useAppTheme();
 
-    const COLOR = theme.colors.error;
+    const COLOR = theme.colors.brand;
     const size = 100;
     const iconName = 'medical-bag';
 
     return (
         <View style={styles.container}>
             <Icon size={size} source={iconName} color={COLOR} />
-            <Text variant="labelLarge" style={{ color: theme.colors.error }}>
+            <Text variant="labelLarge" style={{ color: theme.colors.brand }}>
                 You dont have any planned medications
             </Text>
 
--- src/components/EmptyStockMsg.tsx
+++ src/components/EmptyStockMsg.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Icon, Text, useTheme } from 'react-native-paper';
+import { Icon, Text } from 'react-native-paper';
+import { useAppTheme } from '../theme';
 
 export const EmptyStockMsg = () => {
-    const theme = useTheme();
+    const theme = useAppTheme();
 
-    const COLOR = theme.colors.error;
+    const COLOR = theme.colors.brand;
     const size = 100;
     const iconName = 'medical-bag';
 
     return (
         <View style={styles.emptyStockContainer}>
             <Icon size={size} source={iconName} color={COLOR} />
-            <Text variant="labelLarge" style={{ color: theme.colors.error }}>
+            <Text variant="labelLarge" style={{ color: theme.colors.brand }}>
                 You dont have any medications
             </Text>
         </View>
